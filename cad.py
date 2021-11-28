f4862e4d2536f4390da09c78e731e4d0b451f195
Yokozuna59 <s202163910@kfupm.edu.sa>
2021-11-28 23:18:07 +0300

    IT'S WORKING!

--- cad.py
+++ cad.py
@@ -1,15 +1,37 @@
 # Open and Read the file
-html_page = (open("202110_CACAD_PREP.html")).read()
+from os import X_OK
+
+
+html_page = (open("202120_CACAD_PREP.html")).read()
 
 # Gitting the area were the table is
-table = html_page.replace('<table class="table table-striped" cellspacing="0" rules="all" border="1" id="CntntPlcHldr_grdAcadClndr" style="border-collapse:collapse;">', "$$$").split("$$$")
-table = table[1]
+table = html_page.replace('<table class="table table-striped" cellspacing="0" rules="all" border="1" id="CntntPlcHldr_grdAcadClndr" style="border-collapse:collapse;">', "$$$").replace("</table>", "$$$").split("$$$")
+if (len(table) == 5):
+     ran = 4
+     x = True
+elif (len(table)):
+     ran = 3
+     x = False
 
 # Splitting each line
-lines = table.split("</tr><tr>")
-
-for i in range(len(lines)):
-     splitted = lines[i].replace("<a>", "$$$").replace("</td>", "$$$").replace('">', "$$$").replace("</a>", "$$$").split("$$$")
-     for j in range(len(splitted)):
-          if (j == 7 or j== 10):
-               print(splitted[j])
+for n in range(2, ran):
+     table_e = table[n]
+     lines = table_e.split("</tr><tr>")
+     for i in range(len(lines)):
+          splitted = lines[i].replace("<a>", "$$$").replace("</td>", "$$$").replace('">', "$$$").replace("</a>", "$$$").split("$$$")
+          leng = len(splitted)
+          rang = range(leng)
+          for j in rang:
+               if (leng == 13):
+                    if (j == 7 or j == 10):
+                         print(splitted[j])
+               else:
+                    if x == False:
+                         if (j == 5 or j == 8):
+                              if x == True:
+                                   x = False
+                              elif x == False:
+                                   print(splitted[j])
+                    else:
+                         if (j == 5 or j == 8):
+                              print(splitted[j])
--- tt.py
+++ tt.py
@@ -1,13 +1,44 @@
-from os import read
-from PIL import Image
-import pytesseract
-from bs4 import BeautifulSoup
+# Open and Read the file
+html_page = (open("202110_CACAD_PREP.html")).read()
 
-text = pytesseract.image_to_string(r"port2.jpg")
-text1 = text.replace("Sunday", "").replace("Monday", "").replace("Tuesday", "").replace("Wednesday", "").replace("Thursday","")
+# Gitting the table
+table = html_page.replace('\t\t\t<th scope="col" style="color:White;background-color:#007D40;">DAY</th><th scope="col" style="color:White;background-color:#007D40;">WEEK</th><th scope="col" style="color:White;background-color:#007D40;">HIJRI DATE</th><th scope="col" style="color:White;background-color:#007D40;">GREGORIAN DATE</th><th scope="col" style="color:White;background-color:#007D40;">EVENTS</th>\n\t\t</tr><tr>', "$$$").replace("</table>", "$$$").split("$$$")
 
+# Counting elements
+length_table = len(table)
+if (length_table == 4):
+     range_table = 3
+     bool = False
+elif (length_table == 6):
+     range_table = 5
+     bool = True
 
-ttttt = (open("Weekly Schedule.html", mode='r', encoding='utf-8')).read()
-tt1 = ttttt.replace(">&nbsp;</td>\n</tr>\n<tr>\n<th rowspan=","$$$").split("$$$")
-print(tt1[5])
-# .replace(">&nbsp;</td>", "").replace("<tr>", "").replace("</tr>", "").replace("dddefault", "")
+for n in range(2, range_table):
+     # Getting item
+     table_item = table[n]
+
+     # Splitting each line
+     splitted_lines = table_item.replace("\t", "").replace("<tbody><tr>", "").split("</tr><tr>")
+
+     # Getting each splitted line
+     for i in range(len(splitted_lines)):
+          splitted = splitted_lines[i].replace("<a>", "$$$").replace("</td>", "$$$").replace('">', "$$$").replace("</a>", "$$$").split("$$$")
+          length_splitted = len(splitted)
+          range_splitted = range(length_splitted)
+          for j in range_splitted:
+               if (length_splitted == 13):
+                    if (j == 7 or j == 10):
+                         print(splitted[j])
+               else:
+                    if bool == False:
+                         if (j == 5 or j == 8):
+                              if bool == True:
+                                   bool = False
+                              elif bool == False:
+                                   print(splitted[j])
+                    else:
+                         if (j == 5 or j == 8):
+                              if (splitted[j] == '\n\n'):
+                                   continue
+                              else:
+                                   print(splitted[j])
