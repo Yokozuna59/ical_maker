c77f57666da8553a540a929a35d868ddaed9d406
Yokozuna59 <s202163910@kfupm.edu.sa>
2021-12-01 02:47:11 +0300

    less lines and good indentation

--- cad.py
+++ cad.py
@@ -1,27 +1,22 @@
-# Importing libraries
-from os import close
-from typing import ContextManager
-
+# Creating lists for file path
 first_file = ["CURRENT", "PAST"]
-range_length_first_file = range(len(first_file))
-
-year = ["2021", "2020", "2019"]
-range_length_year = range(len(year))
-
+years = ["2021", "2020", "2019"]
 second_file = ["ACADEMIC", "PREP"]
+terms = ["10","20","30"]
+
+# Gitting the range of the lists
+range_length_first_file = range(len(first_file))
+range_length_year = range(len(years))
 range_length_second_file = range(len(second_file))
+range_length_term = range(len(terms))
 
-term = ["10","20","30"]
-range_length_term = range(len(term))
-
-halfs = ["FIRST HALF", "SECOND HALF"]
-
+# The bool statement for the gaps
 gap_line = False
 
 for f in range_length_first_file:
      new_first_file = first_file[f]
      for y in range_length_year:
-          new_year = year[y]
+          new_year = years[y]
           for s in range_length_second_file:
                new_second_file = second_file[s]
                if (new_first_file == "CURRENT" and new_year != "2021"):
@@ -30,76 +25,148 @@ for f in range_length_first_file:
                     continue
                else:
                     for t in range_length_term:
-                         new_term = term[t]
+                         new_term = terms[t]
                          if (new_second_file == "PREP" and new_term == "30"):
                               continue
                          else:
-                              open_read_page = (open(new_first_file + "/" + new_second_file + "/" + new_year + new_term + ".html")).read()
+                              # Openning and reading the file
+                              open_read_file = (open(new_first_file + "/" + new_second_file + "/" + new_year + new_term + ".html")).read()
 
-                              # Gitting the table
-                              table = open_read_page.replace('\t\t\t<th scope="col" style="color:White;background-color:#007D40;">WEEK DAY</th><th scope="col" style="color:White;background-color:#007D40;">HIJRI DATE</th><th scope="col" style="color:White;background-color:#007D40;">GREGORIAN DATE</th><th scope="col" style="color:White;background-color:#007D40;">EVENTS</th>\n\t\t</tr><tr>', "$$$").replace('\t\t\t<th scope="col" style="color:White;background-color:#007D40;">DAY</th><th scope="col" style="color:White;background-color:#007D40;">WEEK</th><th scope="col" style="color:White;background-color:#007D40;">HIJRI DATE</th><th scope="col" style="color:White;background-color:#007D40;">GREGORIAN DATE</th><th scope="col" style="color:White;background-color:#007D40;">EVENTS</th>\n\t\t</tr><tr>', "$$$").replace("</table>", "$$$").split("$$$")
+                              # Bringing the whole table
+                              table = open_read_file.replace('\t\t\t<th scope="col" style="color:White;background-color:#007D40;">WEEK DAY</th><th scope="col" style="color:White;background-color:#007D40;">HIJRI DATE</th><th scope="col" style="color:White;background-color:#007D40;">GREGORIAN DATE</th><th scope="col" style="color:White;background-color:#007D40;">EVENTS</th>\n\t\t</tr><tr>', "$$$").replace('\t\t\t<th scope="col" style="color:White;background-color:#007D40;">DAY</th><th scope="col" style="color:White;background-color:#007D40;">WEEK</th><th scope="col" style="color:White;background-color:#007D40;">HIJRI DATE</th><th scope="col" style="color:White;background-color:#007D40;">GREGORIAN DATE</th><th scope="col" style="color:White;background-color:#007D40;">EVENTS</th>\n\t\t</tr><tr>', "$$$").replace("</table>", "$$$").split("$$$")
 
-                              # Counting elements
-                              length_table = len(table)
-                              range_table = length_table - 1
+                              # Counting elements and gitting the needed area
+                              length_table = len(table) - 1
+                              range_table = range(2, length_table)
 
+                              # Checking the gap bool statement
                               if (gap_line == False):
                                    gap_line = True
                               else:
                                    print("\n", end= "")
+
+                              # Printing the second path with year
                               print(new_second_file + ", " + new_year + new_term)
-                              half = 0
-                              begin = False
+                              half = False
 
-                              for n in range(2, range_table):
-                                   # Getting item
-                                   table_item = table[n]
+                              for i in range_table:
+                                   # Getting the table
+                                   new_table = table[i]
 
-                                   # Splitting each line
-                                   splitted_lines = table_item.replace("\t", "").replace("<tbody><tr>", "").split("</tr><tr>")
+                                   # Creating bool statements for starting and ending
+                                   start = False
+                                   end = False
+
+                                   # Splitting each line for table
+                                   splitted_lines = new_table.replace("\t", "").replace("<tbody><tr>", "").split("</tr><tr>")
                                    length_splitted_lines = len(splitted_lines)
+                                   range_splitted_lines = range(length_splitted_lines)
 
                                    # Getting each splitted line
-                                   for i in range(length_splitted_lines):
-                                        splitted = splitted_lines[i].replace("<a>", "$$$").replace("</td>", "$$$").replace('">', "$$$").replace("</a>", "$$$").replace("\n", "").split("$$$")
-                                        length_splitted = len(splitted)
-                                        range_splitted = range(length_splitted)
+                                   for j in range_splitted_lines:
+                                        splitted_items = splitted_lines[j].replace("<a>", "$$$").replace("</td>", "$$$").replace('">', "$$$").replace("</a>", "$$$").replace("\n", "").split("$$$")
+                                        length_splitted = len(splitted_items)
+
+                                        # Creating variable for date and event
+                                        if (length_splitted == 11):
+                                             date_item = splitted_items[5]
+                                             event_item = splitted_items[8]
+                                        elif (length_splitted == 13):
+                                             date_item = splitted_items[7]
+                                             event_item = splitted_items[10]
 
                                         if new_second_file == "PREP":
-                                             if half == 0:
-                                                  print("\t" + halfs[half])
-                                                  half = 1
+                                             if half == False:
+                                                  print("\tFIRST HALF")
+                                                  half += 1
                                              elif half == 1 and length_splitted_lines == 1:
-                                                  print("\t" + halfs[half])
-                                                  half = 2
-                                             if (length_splitted != 12):
-                                                  if (length_splitted == 13 and i == 0):
-                                                       pass
-                                                  else:
-                                                       print("\t", end= "")
-                                        if (length_splitted == 11):
-                                             if begin == False:
-                                                  if (splitted[8].find("Classes begin") != -1):
-                                                       print("\t" + splitted[5], end= "")
-                                                       print(", " + splitted[8])
-                                                       begin = True
-                                                  else:
-                                                       if begin == True:
-                                                            print("\t" + splitted[5], end= "")
-                                                            print(", " + splitted[8])
-                                             else:
-                                                  print("\t" + splitted[5], end= "")
-                                                  print(", " + splitted[8])
-                                        elif (length_splitted == 13):
-                                             if begin == False:
-                                                  if (splitted[10].find("Classes begin") != -1):
-                                                       print("\t" + splitted[7], end= "")
-                                                       print(", " + splitted[10])
-                                                       begin = True
-                                                  else:
-                                                       if begin == True:
-                                                            print("\t" + splitted[7], end= "")
-                                                            print(", " + splitted[10])
-                                             else:
-                                                  print("\t" + splitted[7], end= "")
-                                                  print(", " + splitted[10])
+                                                  print("\tSECOND HALF")
+                                                  half += 1
+
+                                        if (length_splitted == 11 and end == False):
+                                             if start == False:
+                                                  if (event_item.find("Classes begin") != -1):
+                                                       if (half == 1 or half == 2):
+                                                            print("\t", end= "")
+                                                       else:
+                                                            pass
+                                                       print("\t" + date_item + ", " + event_item)
+                                                       start = True
+                                             elif start == True:
+                                                  if (event_item.find("Last day before") != -1):
+                                                       continue
+                                                  if (event_item.find("Holiday") != -1):
+                                                       if (half == 1 or half == 2):
+                                                            print("\t", end= "")
+                                                       else:
+                                                            pass
+                                                       print("\t" + date_item + ", " + event_item)
+                                                  elif (event_item.find("resume") != -1):
+                                                       if (half == 1 or half == 2):
+                                                            print("\t", end= "")
+                                                       else:
+                                                            pass
+                                                       print("\t" + date_item + ", " + event_item)
+                                                  elif (event_item.find("Break") != -1):
+                                                       if (half == 1 or half == 2):
+                                                            print("\t", end= "")
+                                                       else:
+                                                            pass
+                                                       print("\t" + date_item + ", " + event_item)
+                                                  elif (event_item.find("Normal") != -1):
+                                                       if (half == 1 or half == 2):
+                                                            print("\t", end= "")
+                                                       else:
+                                                            pass
+                                                       print("\t" + date_item + ", " + event_item)
+                                                  elif (event_item.find("Last day of classes") != -1):
+                                                       if (half == 1 or half == 2):
+                                                            print("\t", end= "")
+                                                       else:
+                                                            pass
+                                                       print("\t" + date_item + ", " + event_item)
+                                                       end = True
+
+                                        elif (length_splitted == 13 and end == False):
+                                             if start == False:
+                                                  if (event_item.find("Classes begin") != -1):
+                                                       if (half == 1 or half == 2):
+                                                            print("\t", end= "")
+                                                       else:
+                                                            pass
+                                                       print("\t" + date_item + ", " + event_item)
+                                                       start = True
+                                             elif start == True:
+                                                  if (event_item.find("Last day before") != -1):
+                                                       continue
+                                                  if (event_item.find("Holiday") != -1):
+                                                       if (half == 1 or half == 2):
+                                                            print("\t", end= "")
+                                                       else:
+                                                            pass
+                                                       print("\t" + date_item + ", " + event_item)
+                                                  elif (event_item.find("Break") != -1):
+                                                       if (half == 1 or half == 2):
+                                                            print("\t", end= "")
+                                                       else:
+                                                            pass
+                                                       print("\t" + date_item + ", " + event_item)
+                                                  elif (event_item.find("resume") != -1):
+                                                       if (half == 1 or half == 2):
+                                                            print("\t", end= "")
+                                                       else:
+                                                            pass
+                                                       print("\t" + date_item + ", " + event_item)
+                                                  elif (event_item.find("Normal") != -1):
+                                                       if (half == 1 or half == 2):
+                                                            print("\t", end= "")
+                                                       else:
+                                                            pass
+                                                       print("\t" + date_item + ", " + event_item)
+                                                  elif (event_item.find("Last day of classes") != -1):
+                                                       if (half == 1 or half == 2):
+                                                            print("\t", end= "")
+                                                       else:
+                                                            pass
+                                                       print("\t" + date_item + ", " + event_item)
+                                                       end = True
