0bbd2a4f7097104c4fc91f838b27ccacb83def31
Yokozuna59 <s202163910@kfupm.edu.sa>
2021-12-04 21:59:30 +0300

    README.md

---
+++ sele.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import Chrome
+
+driver = Chrome()
+driver.get("https://registrar.kfupm.edu.sa/")
