cce6e1f65450491573dfdf4cdcce1589a905258e
Yokozuna59 <s202163910@kfupm.edu.sa>
2021-12-21 22:15:42 +0300

    current

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 # import modules
+from os import replace
+import sys
 import requests
 import ast
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, element
 import json
+
+
+def get_argv():
+     if (len(sys.argv) != 1):
+          next_main = sys.argv[1::]
+          argv = []
+          for i in next_main:
+               argv.append(i)
+     else:
+          argv = None
+     return argv
+argv = get_argv()
 
 def get_urls():
      urls = ["https://registrar.kfupm.edu.sa/CurrentAcadYear", "https://registrar.kfupm.edu.sa/PastAcadYear"]
      return urls
+urls = get_urls()
 
+def days_month(month):
+     days_month = ["31", "28", "31", "30", "31", "30", "31", "31", "30", "31", "30", "31"]
+     return days_month[month]
 
 def get_payloads():
      current_acad = ast.literal_eval(open("payloads/current/acad.json").read())
@@ -16,12 +34,10 @@ def get_payloads():
      past_acad = ast.literal_eval(open("payloads/past/acad.json").read())
      past_prep = ast.literal_eval(open("payloads/past/prep.json").read())
      return current_acad, current_prep, past_acad, past_prep
+payloads = get_payloads()
 
 
 def get_terms():
-     urls = get_urls()
-     payloads = get_payloads()
-
      current_acad_terms = []
      current_prep_terms = []
      past_acad_terms = []
@@ -34,34 +50,49 @@ def get_terms():
                options = soup.find_all('option')
 
                for option in options:
-                    if (option["value"].find("0") != 0):
-                         if (url.find("Current") != -1):
-                              if (option.text.find("Prep") != -1):
-                                   current_prep_terms.append(option["value"])
-                              else:
-                                   current_acad_terms.append(option["value"])
-                         elif (url.find("Past") != -1):
-                              if (option.text.find("Prep") != -1):
-                                   past_prep_terms.append(option["value"])
-                              else:
-                                   past_acad_terms.append(option["value"])
+                    value = option["value"]
+                    if (argv != None):
+                         for i in argv:
+                              if (value.find(i) != -1):
+                                   if (value.find("0") != 0):
+                                        if (url.find("Current") != -1):
+                                             if (option.text.find("Prep") != -1):
+                                                  current_prep_terms.append(value)
+                                             else:
+                                                  current_acad_terms.append(value)
+                                        elif (url.find("Past") != -1):
+                                             if (option.text.find("Prep") != -1):
+                                                  past_prep_terms.append(value)
+                                             else:
+                                                  past_acad_terms.append(value)
+                    else:
+                         if (value.find("0") != 0):
+                              if (url.find("Current") != -1):
+                                   if (option.text.find("Prep") != -1):
+                                        current_prep_terms.append(value)
+                                   else:
+                                        current_acad_terms.append(value)
+                              elif (url.find("Past") != -1):
+                                   if (option.text.find("Prep") != -1):
+                                        past_prep_terms.append(value)
+                                   else:
+                                        past_acad_terms.append(value)
      return current_acad_terms, current_prep_terms, past_acad_terms, past_prep_terms
+terms = get_terms()
 
 
-def table():
-     urls = get_urls()
-     terms = get_terms()
-     payloads = get_payloads()
+def get_tables():
+     tables = []
 
      for url in urls:
           for index_payload in range(len(payloads)):
                for page in range(len(terms)):
                     if (url.find("Current") != -1):
-                         if (page == 2):
+                         if (page >= 2):
                               break
                     elif (url.find("Past") != -1):
-                         page = 2
-                         index_payload = 2
+                         if (index_payload < 2):
+                              continue
                     if (page == index_payload):
                          payload = payloads[index_payload]
                          last_key = list(payload.keys())[-1]
@@ -70,18 +101,190 @@ def table():
                               html = (requests.post(url, data = payload)).text
                               soup = BeautifulSoup(html, 'html.parser')
                               table = soup.find_all(class_ = "table-responsive")
-                              print(table)
-
-table()
-
-
+                              tables.append(table)
+     return tables
+tables = get_tables()
 
 
+def get_global_terms():
+
+     global_terms = {}
+
+     for table in tables:
+          soup = BeautifulSoup(str(table), 'html.parser')
+          if (len(soup.text) != 4):
+               href = soup.find("a").get("href")
+               href_term = href.replace("_", "/").replace(".", "/").split("/")
+               elements = href_term[4:len(href_term)-1]
+               term = elements[0][2:5]
+
+               keys = list(global_terms.keys())
+               if ((len(keys)) == 0):
+                    global_terms[term] = {}
+               for i in (keys):
+                    if (term.find(i) != -1):
+                         continue
+                    else:
+                         global_terms[term] = {}
+               term_acad = global_terms[term]
+               term_acad["ACAD"] = {"INCLUDE":{},"EXCLUDE":{}}
+               if (len(elements) == 3):
+                    term_acad["PREP"] = {"FIRST":{"INCLUDE":{},"EXCLUDE":{}},"SECOND":{"INCLUDE":{},"EXCLUDE":{}}}
+
+     return global_terms
+global_terms = get_global_terms()
 
 
-def days_month(month):
-     days_month = ["31", "28", "31", "30", "31", "30", "31", "31", "30", "31", "30", "31"]
-     return days_month[month]
+def replacement(i):
+     replace_from = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+     replace_to = ["01/", "02/", "03/", "04/", "05/", "06/", "07/", "08/", "09/", "10/", "11/", "12/", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]
+     return replace_from[i], replace_to[i]
+
+
+def row():
+     for table in tables:
+          soup = BeautifulSoup(str(table), 'html.parser')
+          if (len(soup.text) != 4):
+               href = soup.find("a").get("href")
+               href_term = href.replace("_", "/").replace(".", "/").split("/")
+               elements = href_term[4:len(href_term)-1]
+               term = elements[0][2:5]
+               if (len(elements) != 3):
+                    dictionary = global_terms[term]["ACAD"]
+               else:
+                    dictionary = global_terms[term]["PREP"]
+
+               first_line = False
+               needed_events = []
+               columns = soup.find_all("tr")
+               for column in columns:
+                    rows = column.find_all('td')
+
+                    if (len(rows) != 0):
+                         if (len(rows) == 5):
+                              row = 3
+                         else:
+                              row = 2
+                         full_date = " ".join(((rows[row].text).lower()).split())
+                         event = " ".join(((rows[row + 1].text).lower()).split())
+
+                         # use the current index and replace word from the replacement lists
+                         for i in range(38):
+                              rep = replacement(i)
+                              full_date = full_date.replace(str(rep[0]), str(rep[1]))
+
+                         if (first_line == False):
+                              splitted_full_date = full_date.replace("-", " ").split()
+                              year = splitted_full_date[-1]
+                              next_year = str(int(year) + 1)
+                              first_line = True
+                         elif (full_date.find(next_year) != -1):
+                              year = next_year
+
+                         if (event.find("last day before") != -1) or (event.find("exams preparation break") != -1):
+                              continue
+                         elif (event.find("classes begin") != -1) or(event.find("holiday") != -1) or (event.find("resume") != -1) or (event.find("break") != -1) or (event.find("normal") != -1) or (event.find("last day of classes") != -1):
+                              splitted_full_date = (full_date.replace("-", " - ").split())
+
+                              dates = []
+
+                              for k in splitted_full_date[::-1]:
+                                   # check if full date contains (year)
+                                   if (year == k):
+                                        dates.append(k)
+                                   else:
+                                        if ((len(k) == 1) and (k != "-")):
+                                             k = '0' + k
+                                        dates.append(k)
+
+                                   full_date = "".join(dates)
+                                   if (full_date.find(year) != -1):
+                                        pass
+                                   else:
+                                        full_date = "".join(year + full_date)
+
+                              if (((full_date.find("-") != -1) or (event.find("resume") != -1))):
+                                   print(full_date.replace("/", " ").replace("-", " ").split()[::-1])
+                                   split_by_slash = (full_date[4::].replace("/", " ").replace("-", " ").split()[::-1])
+                                   print(split_by_slash)
+                                   if (len(split_by_slash) == 2):
+                                        before_resume = (needed_events[-1].split(", "))
+                                        day_before_resume = before_resume[0][6::]
+                                        month_before_resume = before_resume[0][4:6]
+                                        event_before_resume = before_resume[1]
+
+                                        month = split_by_slash[1]
+                                        if (month_before_resume == month):
+                                             for k in range(int(day_before_resume) + 1, int(split_by_slash[0])):
+                                                  k = str(k)
+                                                  if ((len(k) == 1)):
+                                                       k = '0' + k
+                                                  needed_events.append(full_date[0:6] + k + ", " + event_before_resume)
+                                        else:
+                                             for k in (0,1):
+                                                  if (k == 0):
+                                                       month = month_before_resume
+                                                       days = day_before_resume
+                                                       month_days = days_month(int(month_before_resume) - 1)
+                                                  else:
+                                                       month = split_by_slash[1]
+                                                       days = 1
+                                                       month_days = split_by_slash[0]
+
+                                                  for k in range(int(days) + 1, int(month_days) + 1):
+                                                       k = str(k)
+                                                       if ((len(k) == 1)):
+                                                            k = '0' + k
+                                                       needed_events.append(full_date[0:4] + month + k + ", " + event_before_resume)
+                                        full_date = full_date.replace("/", "")
+                                        needed_events.append(full_date + ", " + event)
+
+                                   elif len(split_by_slash) == 3:
+                                        for k in range(int(split_by_slash[0]), int(split_by_slash[1]) + 1):
+                                             k = str(k)
+                                             if ((len(k) == 1)):
+                                                  k = '0' + k
+                                             needed_events.append(full_date[0:6] + k + ", " + event_before_resume)
+                                   elif (len(split_by_slash) == 4):
+                                        for k in (0,2):
+                                             month = split_by_slash[k + 1]
+                                             if (k == 2):
+                                                  days = 1
+                                                  month_days = split_by_slash[k]
+                                             else:
+                                                  days = split_by_slash[k]
+                                                  month_days = days_month(int(month) - 1)
+
+                                             for k in range(int(days) , int(month_days) + 1):
+                                                  k = str(k)
+                                                  if ((len(k) == 1)):
+                                                       k = '0' + k
+                                                  needed_events.append(full_date[0:4] + month + k + ", " + event)
+                              else:
+                                   full_date = full_date.replace("/", "")
+                                   needed_events.append(full_date + ", " + event)
+               print(needed_events)
+
+row()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 def main2():
